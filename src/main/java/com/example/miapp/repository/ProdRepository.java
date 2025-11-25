4e1394c1eaa7c850432a2e58b431032109e07734
alan-flores1 <ala.floresr@duocuc.cl>
2025-11-24 22:26:22 -0300

    :(

--- src/main/java/com/example/miapp/repository/ProdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.miapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.miapp.model.Producto;
-
-public interface ProdRepository extends JpaRepository<Producto, Long> {
-}
