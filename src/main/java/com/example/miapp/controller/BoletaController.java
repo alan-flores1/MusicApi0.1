c6827712f83ffc5530af06edb31eb8f793a1145a
alan-flores1 <ala.floresr@duocuc.cl>
2025-11-24 23:22:37 -0300

    afada

--- src/main/java/com/example/miapp/controller/BoletaController.java
+++ src/main/java/com/example/miapp/controller/BoletaController.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/pagos")
+@RequestMapping("/api/boletas")
 @CrossOrigin(origins = "*")
 @Tag(name = "Boletas / Pagos", description = "Operaciones para registrar y consultar pagos y boletas")
 public class BoletaController {
